706099ac05f5c63e000c1d77dfacfa51106f04bb
Evelyn leal <evelynleal211@gmail.com>
2026-03-24 20:12:59 -0300

    atualiza projeto apos aula de 24/03

--- scripts/01-introducao.R
+++ scripts/01-introducao.R
@@ -2,8 +2,8 @@
 # DOCUMENTACAO DO SCRIPT
 # ============================================================
 # Arquivo: 01_introducao.R
-# Autor(a): <seu nome>
-# Data: <dd/mm/aaaa>
+# Autor(a): <Evelyn Leal>
+# Data: <07/11/2006>
 # Objetivo: entender os fundamentos da linguagem R
 
 
@@ -17,9 +17,10 @@
 Sys.setlocale("LC_ALL", "pt_BR.UTF-8")
 
 
-# ==========================================
-# BLOCO 1 - R como uma grande calculadora
-# ==========================================
+
+#  BLOCO 1 - R como uma grande calculadora --------------------------------
+
+
 
 # Carrega o pacote se ele tiver sido instalado
 library(tidyverse)
@@ -73,9 +74,10 @@ sqrt(225)
 round(3.14159, digits = 2)
 
 
-# ============================================================
-# BLOCO 2 - Tipos Atômicos e classes
-# ============================================================
+
+# BLOCO 2 - Tipos Atômicos e classes --------------------------------------
+
+
 
 # Os tipos de dados definem como os dados
 # são armazenados na memória.
@@ -113,9 +115,10 @@ f <- as.numeric(c)
 f
 
 
-# ============================================================
-# BLOCO 3 - Vetores numericos e vetorizacao
-# ============================================================
+
+# BLOCO 3 - Vetores numericos e vetorizacao -------------------------------
+
+
 
 # Atalho de teclado para inserir <- :
 # Alt + - (Windows/Linux)
@@ -167,9 +170,9 @@ max(receita_diaria)
 ?length
 
 
-# ============================================================
-# BLOCO 4 - Vetores
-# ============================================================
+
+# BLOCO 4 - Vetores -------------------------------------------------------
+
 
 # vetor de caracteres (strings) com nome da empresa
 nome_empresa <- c("Loja A", "Loja B", "Loja C")
@@ -186,10 +189,9 @@ meta_batida
 class(meta_batida)
 
 
-# ============================================================
-# BLOCO 5 - Criando uma tibble com dados ficticios de vendas
-# e primeiros exemplos com dplyr
-# ============================================================
+# BLOCO 5 - Criando uma tibble com dados ficticios de vendas  e pr --------
+
+
 
 # Neste bloco vamos criar um pequeno conjunto de dados
 # semelhante a um banco de dados simples de vendas.
@@ -306,9 +308,9 @@ vendas_diarias |>
 vendas_diarias |>
   filter(between(receita, 10000, 11500))
 
-# ============================================================
-# BLOCO 6 - Manipulação de dados com dplyr e uso do pipe
-# ============================================================
+
+# BLOCO 6 - Manipulação de dados com dplyr e uso do pipe ------------------
+
 
 # O operador pipe |> permite encadear operações
 # de forma mais legível.
@@ -323,25 +325,6 @@ vendas_diarias |>
 #   funcao1() |>
 #   funcao2() |>
 #   funcao3()
-
-
-# ------------------------------------------------------------
-# Boas práticas no RStudio
-# ------------------------------------------------------------
-
-# LEMBRAR DE MOSTRAR PARA OS ALUNOS:
-
-# 1) Configurar o RStudio para usar o pipe nativo |>
-
-# 2) Atalho de teclado para inserir pipe
-# Ctrl + Shift + M (Windows/Linux)
-
-# 3) Formatar código automaticamente
-# Code -> Reformat Selection
-
-# 4) (OPCIPONAL) Mostrar como reorganizar os painéis do RStudio
-# Console, Environment, Source, Files etc.
-
 
 # ------------------------------------------------------------
 # Exemplo 1 - selecionar e ordenar dados
@@ -409,9 +392,9 @@ vendas_diarias |>
   arrange(desc(receita_media))
 
 
-# ============================================================
-# BLOCO 7 - Visualizacao de dados com ggplot2
-# ============================================================
+
+# BLOCO 7 - Visualizacao de dados com ggplot2 -----------------------------
+
 
 # Grafico 1 de barras da receita media por loja
 ggplot(receita_por_loja, aes(x = loja, y = receita_media)) +
@@ -447,9 +430,10 @@ ggplot(vendas_diarias, aes(x = data, y = lucro, size = receita, color = loja)) +
   geom_point()
 
 
-# ============================================================
-# BLOCO 8 - Resolução dos exercícios propostos nos slides
-# ============================================================
+
+
+#  BLOCO 8 - Resolução dos exercícios propostos nos slides ----------------
+
 
 
 # Solução do Ex. 1
